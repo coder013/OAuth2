16cb4cd9808b9d503dc66527dbd0d5911ad09a7e
coder013 <wnssh607@gmail.com>
2023-01-04 00:59:21 +0900

    Spring Boot with OAuth 2.0 프로젝트 생성

---
+++ src/main/java/com/coder013/oauth2/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.coder013.oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .cors().disable()
+                .csrf().disable()
+                .build();
+    }
+}
